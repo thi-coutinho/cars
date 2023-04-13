ab37a2f0f40097d31cf6ed3d58a9d2a7b365c5a4
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-04-13 11:08:48 -0300

    rafactor: change db to prisma

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,11 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-
-const { Pool } = pg;
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL
-};
-
-const db = new Pool(configDatabase);
-export default db;
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
+const prisma = new PrismaClient()
+// prisma.$connect()
+// try {
+//     const getconnect = async () => await prisma.$connect()
+//     getconnect()
+// } catch (error) {
+//     console.log(error)
+// }
+export default prisma
